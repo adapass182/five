70853185263ed2f04e3318cafa986d1d5e1a5a34
Adam Passingham <adampassingham90@gmail.com>
2022-12-21 17:33:04 +0100

    refactor: refactor build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve'; // locate and bundle dependencies in node_modules (mandatory)
 
 export default {
-  input: 'src/main.js',
+  input: './src/main.js',
   output: [
     {
       format: 'umd',
       name: 'Five',
-      file: 'build/bundle.js',
+      file: './build/bundle.js',
     },
   ],
   plugins: [resolve()],
