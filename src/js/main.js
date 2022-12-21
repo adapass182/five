a1790b0242db604bb2430832ca07c846def6a9d3
Adam Passingham <adampassingham90@gmail.com>
2022-12-21 15:02:06 +0100

    refactor: refactor main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import * as THREE from '../../node_modules/three/build/three.module.js';
-// import { TrackballControls } from 'three/TrackballControls';
+// import { TrackballControls } from '../../node_modules/three/examples/jsm/controls/TrackballControls.js';
 
 const { innerHeight, innerWidth } = window;
 
-/* Renderer */
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+/* Scene */
+const scene = new THREE.Scene();
 
 /* Camera */
 const camera = new THREE.PerspectiveCamera(
@@ -15,31 +15,8 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 5; // Offset camera, otherwise camera & box will both initially render at `0, 0, 0`
 
-/* Scene */
-const scene = new THREE.Scene();
-
-/* Lights */
-const lights = [];
-const lightValues = [
-  { colour: 0x14d14a, intensity: 8, dist: 12, x: 1, y: 0, z: 8 },
-  { colour: 0xbe61cf, intensity: 6, dist: 12, x: -2, y: 1, z: -10 },
-  { colour: 0x00ffff, intensity: 3, dist: 10, x: 0, y: 10, z: 1 },
-  { colour: 0x00ff00, intensity: 6, dist: 12, x: 0, y: -10, z: -1 },
-  { colour: 0x16a7f5, intensity: 6, dist: 12, x: 10, y: 3, z: 0 },
-  { colour: 0x90f615, intensity: 6, dist: 12, x: -10, y: -1, z: 0 },
-];
-
-lightValues.forEach((value) => {
-  const lightValue = new THREE.PointLight(
-    value.colour,
-    value.intensity,
-    value.dist,
-  );
-  lightValue.position.set(value.x, value.y, value.z);
-  scene.add(lightValue);
-});
-
-export default lights;
+/* Renderer */
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setClearColor('#233143'); // Sets background color
 renderer.setSize(innerWidth, innerHeight); // Sets size of 'app' - this will fill the screen (matches aspect ratio)
@@ -62,8 +39,35 @@ boxMesh.rotation.set(40, 0, 40); // Change initial rotation of box (x, y, z)
 
 scene.add(boxMesh);
 
+/* Lights */
+const lightValues = [
+  { colour: 0x14d14a, intensity: 8, dist: 12, x: 1, y: 0, z: 8 },
+  { colour: 0xbe61cf, intensity: 6, dist: 12, x: -2, y: 1, z: -10 },
+  { colour: 0x00ffff, intensity: 3, dist: 10, x: 0, y: 10, z: 1 },
+  { colour: 0x00ff00, intensity: 6, dist: 12, x: 0, y: -10, z: -1 },
+  { colour: 0x16a7f5, intensity: 6, dist: 12, x: 10, y: 3, z: 0 },
+  { colour: 0x90f615, intensity: 6, dist: 12, x: -10, y: -1, z: 0 },
+];
+
+lightValues.forEach((value) => {
+  const lightValue = new THREE.PointLight(
+    value.colour,
+    value.intensity,
+    value.dist,
+  );
+  lightValue.position.set(value.x, value.y, value.z);
+  scene.add(lightValue);
+});
+
+//Trackball Controls for Camera
+// const controls = new TrackballControls(camera, renderer.domElement);
+// controls.rotateSpeed = 4;
+// controls.dynamicDampingFactor = 0.15;
+
 const rendering = () => {
   requestAnimationFrame(rendering);
+
+  // controls.update();
 
   // Spin the box
   scene.rotation.z -= 0.005;
